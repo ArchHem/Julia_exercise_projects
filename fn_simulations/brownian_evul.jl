3e3a745f86b134ed9423a1e7371245cc7ea126eb
ArchHem <84734676+ArchHem@users.noreply.github.com>
2024-02-24 22:09:57 +0000

    Added comparassion code.

--- fn_simulations/brownian_evul.jl
+++ fn_simulations/brownian_evul.jl
@@ -21,7 +21,7 @@ function integrate_brownian_motions(input::BrownianMotion)
     return time_array, total_dat
 end
 
-example_brownian_sim = BrownianMotion(50000,3600,0.1,1.0)
+example_brownian_sim = BrownianMotion(50000,3600,1.0,1.0)
 times, values = integrate_brownian_motions(example_brownian_sim)
 
 N_examps = 30
@@ -33,14 +33,31 @@ skewness = sum(((final_pos .-mu)./sigma).^3)/length(final_pos)
 curtosis = sum(((final_pos .-mu)./sigma).^4)/length(final_pos)
 
 println("Measured position mean: ",mu)
+println("We expected a position mean of 0.0.")
+
 println("Measured position std: ", sigma)
+println("We expected a sigma of ", sqrt(example_brownian_sim.dt * example_brownian_sim.NTimeSteps))
+
+
 println("Measured position skewness: ", skewness)
+println("We expect a skewness of 0.0")
 println("Measured position curtosis: ", curtosis)
+println("We expect a curtosis of ", 3*example_brownian_sim.sigma^2)
 
 #plot0 = plot(times, [values[n,:] for n in 1:N_examps], 
 #title = "Brownian movement examples", legend = false, 
 #linewidth = 0.5, linecolors = colors, xlabel = "Timestep", ylabel = "Position", dpi = 500)
 
 plot1 = plot(histogram(final_pos, color = "green", title = "Final position histogram",
-legend = false, xlabel = "Position", ylabel = "Position counts", bins = 100, dpi = 500))
+legend = false, xlabel = "Position", ylabel = "Position frequencies", bins = 100, dpi = 500, normalize = :pdf, label = ""))
+
+xplot = LinRange(-250,250,1000)
+sigmaplot = sqrt(example_brownian_sim.dt * example_brownian_sim.NTimeSteps)
+yplot = @. exp(-xplot^2/(2*sigmaplot^2))/sqrt(2pi * sigmaplot^2)
+
+plot!(plot1,xplot,yplot, color = "red",ls =:dash, label = "Expected normal distribution", legend = true, linewidth = 2.0)
+
+
+
+
 
